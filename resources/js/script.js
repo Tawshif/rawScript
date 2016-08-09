e52dd82bc8d7a799bdef3eaa658d5280cae1b1da
tawshif <anikphp@gmail.com>
2016-08-09 19:17:36 +0600

    update

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,16 @@
-//  main script file
+window.onload = function () {
+	
+	// document.getElementById('add').onclick;
+
+	document.getElementById('add').addEventListener('click', function (e) {
+		e.preventDefault();
+
+		var value = document.getElementById('item').value;
+
+		if (value) {
+			console.log(value);	
+		} 
+		
+	})
+
+}
